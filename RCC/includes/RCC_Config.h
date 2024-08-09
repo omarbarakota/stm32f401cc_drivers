97222bbe323087f579c9edcbd8543c9f81fa9197
omarbarakota <omar.barkat731@gmail.com>
2024-08-09 20:07:25 +0200

    RCC_Config.h Modified

--- RCC/includes/RCC_Config.h
+++ RCC/includes/RCC_Config.h
@@ -1,54 +1,63 @@
 /*
- * Driver:  RCC_Driver
- * Author:  Omar Barakat
- * Version: V 1.0
- * Date:    Aug, 2023
- * Description:
- * */
-
-#ifndef RCC_CONFIG_H_
-#define RCC_CONFIG_H_
-
-/*System clock SOURCE
-*option=
-*			1- RCC_u32_HSI
-*			2- RCC_u32_HSE
-*			3- RCC_u32_PLL
-*/
-#define RCC_u32_CLOCK_SRC		RCC_u32_HSI
-
-/*in case of using RCC_u32_HSE as system clock source you need to configure the next parameters
- *options:
- *			RCC_u32_HSE_CRYSTAL
- *			RCC_u32_HSE_BYBASS
- */
-#define RCC_u32_HSE_MODE		RCC_u32_HSE_CRYSTAL
-
-
-/*PLL Clock Source
- * Options: 1- RCC_u32_PLL_SRC_HSI_DIV2
- * 			2- RCC_u32_PLL_SRC_HSE
- */
-#define RCC_u32_PLL_SRC		RCC_u32_PLL_SRC_HSI
-
-/*
-//PLL output clock frequency = VCO frequency / PLLP
- * PLLP options:
- * 				 RCC_PLLP2
- * 				 RCC_PLLP4
- * 				 RCC_PLLP6
- * 				 RCC_PLLP8
+ * RCC_Config.h
  *
+ *  Created on: Aug 13, 2023
+ *      Author: Lenovo
  */
-#define RCC_u32_PLLP		RCC_u32_PLLP4
 
+#ifndef MCAL_AUTOSARRCC_RCC_CONFIG_H_
+#define MCAL_AUTOSARRCC_RCC_CONFIG_H_
 
-//VCO output frequency = VCO input frequency × PLLN with range 192MHZ <=VCO  <=432MHZ
-#define RCC_u32_PLLN			192
+#include "RCC_Private.h"
 
+/*The system's clock source options
+ * 1-RCC_HSI
+ * 2-RCC_HSE
+ * 3-RCC_PLL
+ */
+#define RCC_CLK_SRC RCC_HSI
 
-//VCO input frequency = PLL input clock frequency / PLLM with 2 <=PLLM <=
-#define RCC_u32_PLLM			3
+#if RCC_CLK_SRC==RCC_HSE
 
+/* HSE Mode Configurations
+ * 1-RCC_HSE_BYPASS
+ * 2-RCC_HSE_CRYSTAL
+ */
+#define RCC_HSE_MODE	RCC_HSE_CRYSTAL
 
-#endif //RCC_CONFIG_H_
+#elif RCC_CLK_SRC==RCC_PLL
+/*PLL Configurations*/
+/* Equation-> CLK_SRC_OUT=CLK_SRC_IN/M*N/P
+ * Equation-> CLK_SRC_OUT=CLK_SRC_IN/M*N/P
+ * */
+/*PLLSRC options
+ * 1-RCC_PLL_HSI
+ * 2-RCC_PLL_HSE
+ */
+#define RCC_PLL_SRC		RCC_PLL_HSE
+/*PLLP options
+ * Out clock freq=VCO freq/PLLP
+ * 1-RCC_PLL2
+ * 2-RCC_PLL4
+ * 3-RCC_PLL6
+ * 4-RCC_PLL8
+ */
+#define RCC_PLLP	RCC_PLL4
+
+/*PLLM options
+ * 2:63
+ */
+#define RCC_PLLM		4
+
+/*PLLN options
+ * 2:432
+ */
+#define RCC_PLLN		16
+
+/*PLLQ options
+ * 2:15
+ */
+#define RCC_PLLQ		2
+
+#endif	//RCC_CLK_SRC Modes
+#endif /* MCAL_AUTOSARRCC_RCC_CONFIG_H_ */
