ea3301960b822e297524bf0b3e867deaee28dff1
omarbarakota <omar.barkat731@gmail.com>
2024-08-13 19:09:46 +0200

    Systick_Config.h Modified

---
+++ STK/includes/Systick_Config.h
@@ -0,0 +1,17 @@
+/*
+ * Systick_Config.h
+ *
+ *  Created on: Aug 12, 2023
+ *      Author: Barakat
+ */
+
+#ifndef SYSTICK_CONFIG_H_
+#define SYSTICK_CONFIG_H_
+
+/* Systick Clock Options
+ * 1- STK_AHB_CLK
+ * 2- STK_AHB_CLK_DIV_BY_8
+ */
+#define STK_CLK		STK_AHB_CLK_DIV_BY_8
+
+#endif /* SYSTICK_CONFIG_H_ */
