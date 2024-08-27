3691699465c0a62022b61b724ab0bbdf8feba395
omarbarakota <omar.barkat731@gmail.com>
2024-08-27 14:18:25 +0200

    System clock Added for configs

--- STK/includes/Systick_Config.h
+++ STK/includes/Systick_Config.h
@@ -14,4 +14,12 @@
  */
 #define STK_CLK		STK_AHB_CLK_DIV_BY_8
 
+/*
+ * HSI=16000000
+ * HSE=(4:26)000000Mhz
+ * PLL=
+ */
+/*This depends on the RCC System Clock Configurations*/
+#define STK_CLK_Value 16000000
+
 #endif /* SYSTICK_CONFIG_H_ */
