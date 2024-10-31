9455c34418c9c6a360e00c876816f615a5e50756
omarbarakota <omar.barkat731@gmail.com>
2024-10-31 19:46:44 +0300

    GPIO_Config.h Modified

---
+++ GPIO/includes/GPIO_Config.h
@@ -0,0 +1,14 @@
+
+#ifndef GPIO_CONF_H_
+#define GPIO_CONF_H_
+
+#define GPIOA_PNIN0				0
+#define GPIOA_PNIN1             1
+#define GPIOA_PNIN2             2
+#define GPIOA_PNIN3             3
+#define GPIOA_PNIN4             4
+#define GPIOA_PNIN5             5
+#define GPIOA_PNIN6             6
+#define GPIOA_PNIN7             7
+
+#endif /* GPIO_CONF_H_ */
